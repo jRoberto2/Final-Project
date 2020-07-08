a7bd7630e1826af3e21c0da85db2be5ff935d7dc
Julio A. Roberto <jRoberto2@mail.valenciacollege.edu>
2020-07-07 22:41:19 -0400

    Added form to html, defined sections in js

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
 //Final Project
+
+//A new to do should be displayed below any previous to dos
+
+//Every to do in the list should have a check button next to it to check off the to do and mark it complete
+
+//Every to do should have an X to the right that will delete the to do
